426e040cfa0cf33e12e1b0f65ed4b59c04ab204d
diamond-nicholas <diamondnicholas154@gmail.com>
2023-09-09 15:32:44 +0100

    add live connection string

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require("pg");
-const pool = new Pool();
+const pool = new Pool(
+  "postgres://itrends:HwGb4MzgWu5HBwuYd81cLoteIQTqAdaw@dpg-cju80515mpss73c8navg-a.oregon-postgres.render.com/itrends"
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
