231d576475e3f7d9448cba9d0d03c9ff0a82a33a
diamond-nicholas <diamondnicholas154@gmail.com>
2023-09-09 16:46:03 +0100

    update connections

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
 const { Pool } = require("pg");
-const pool = new Pool(
-  "postgres://itrends:HwGb4MzgWu5HBwuYd81cLoteIQTqAdaw@dpg-cju80515mpss73c8navg-a.oregon-postgres.render.com/itrends"
-);
+
+const databaseConfig = {
+  connectionString:
+    "postgres://cmzhebcjxdklny:1377202b2738fe15090fdb7b1364d9e633374017e563ebe7e059d303208c2cd2@ec2-35-169-9-79.compute-1.amazonaws.com:5432/detgkp59l1kqjk",
+  ssl: { rejectUnauthorized: false },
+};
+
+/* const client = new Client(process.env.DATABASE_URL); */
+const pool = new Pool(databaseConfig);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
--- models/index.js
+++ models/index.js
@@ -1,37 +1,42 @@
-'use strict';
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const connectionString =
+  "postgres://cmzhebcjxdklny:1377202b2738fe15090fdb7b1364d9e633374017e563ebe7e059d303208c2cd2@ec2-35-169-9-79.compute-1.amazonaws.com:5432/detgkp59l1kqjk";
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+const sequelize = new Sequelize(connectionString, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
