4ad081773428a5ad3f7325131486761619b9b523
diamond-nicholas <diamondnicholas154@gmail.com>
2023-09-09 17:41:06 +0100

    more modification

--- models/index.js
+++ models/index.js
@@ -6,8 +6,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const db = {};
 
-const connectionString =
-  "postgres://cmzhebcjxdklny:1377202b2738fe15090fdb7b1364d9e633374017e563ebe7e059d303208c2cd2@ec2-35-169-9-79.compute-1.amazonaws.com:5432/detgkp59l1kqjk";
+const connectionString = process.env.DBURI;
 
 const sequelize = new Sequelize(connectionString, {
   dialect: "postgres",
