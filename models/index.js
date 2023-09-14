a9f058f305be9c2670c7d7092d59270fc935ff77
diamond-nicholas <diamondnicholas154@gmail.com>
2023-09-14 21:22:45 +0100

    change db

--- models/index.js
+++ models/index.js
@@ -6,7 +6,9 @@ const config = require("../config/config");
 const basename = path.basename(__filename);
 const db = {};
 
-const connectionString = config.pg.url;
+// const connectionString = config.pg.url;
+const connectionString =
+  "postgres://cmzhebcjxdklny:1377202b2738fe15090fdb7b1364d9e633374017e563ebe7e059d303208c2cd2@ec2-35-169-9-79.compute-1.amazonaws.com:5432/detgkp59l1kqjk";
 
 const sequelize = new Sequelize(connectionString, {
   dialect: "postgres",
