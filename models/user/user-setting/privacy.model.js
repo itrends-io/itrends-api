ce7d844f1bde560d6783a82ff248a039ffb87a3b
diamond-nicholas <diamondnicholas154@gmail.com>
2023-10-11 14:27:52 +0100

    add complete model for privacy

--- models/user/user-setting/privacy.model.js
+++ models/user/user-setting/privacy.model.js
@@ -9,6 +9,58 @@ module.exports = (sequelize) => {
       primaryKey: true,
       unique: true,
     },
+    is_visible_online: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_profile_private: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    show_following_count: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    show_follower_count: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    show_media_count: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_login_redirect: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_allow_suggestions: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    is_want_comment: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_comment_only_for_payers: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    show_post_tips_sum: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_watermark_photo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_watermark_video: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_watermark_text: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   });
   return Privacy;
 };
